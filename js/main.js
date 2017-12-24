d4a7cd6aa8f6a3a4c82ef5801302a1bcbbedc766
shihjian <werty7133025@gmail.com>
2017-12-24 14:54:47 +0800

    YJ

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$("#menubtn").on("click",function(){
-		if($("header > nav").hasClass("open")){
-			$("header > nav").removeClass("open");
-		}else{
-			$("header > nav").addClass("open");
-		}
-	})
-})
